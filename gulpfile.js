5c77f5279e581dde0a2eb894f5d357d383086486
Mathias <tympanix@gmail.com>
2017-08-09 15:36:19 +0200

    Added dist files

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@ const gulp = require('gulp');
 const browsersync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
 const useref = require('gulp-useref');
-
-const OUT = './www';
+const rename = require('gulp-rename');
+const babel = require('gulp-babel');
+const gulpif = require('gulp-if');
+const cleancss = require('gulp-clean-css');
 
 gulp.task('serve', function() {
     browsersync.init({
         server: {
-            baseDir: "./docs"
+            baseDir: "./"
         }
     });
 });
@@ -17,15 +19,27 @@ gulp.task('reload', function () {
     browsersync.reload()
 });
 
-gulp.task('build', function() {
+gulp.task('build', ['build:docs', 'build:dist'])
+
+gulp.task('build:docs', function() {
     return gulp.src('index.html')
         .pipe(useref())
+        .pipe(gulpif('patternlock.js', babel({presets: ['env']})))
         .pipe(gulp.dest('docs'))
-        .pipe(browsersync.reload({stream: true}))
+})
+
+gulp.task('build:dist', function() {
+    return gulp.src('patternlock.*')
+        .pipe(gulpif('*.js', babel({presets: ['env']})))
+        .pipe(gulp.dest('dist'))
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', cleancss()))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'))
 })
 
 gulp.task('watch', function () {
-    gulp.watch(['./*.html', './*.css', './*.js'], ['build']);
+    gulp.watch(['./*.html', './*.css', './*.js'], ['reload']);
 });
 
 gulp.task('default', ['serve', 'watch']);
